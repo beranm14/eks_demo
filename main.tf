cc15dbb21b793e6a5f73d7d9cf099b22e684dfdb
Martin Beránek <martin.beranek@shipmonk.com>
2023-11-16 23:27:15 +0100

    🔒 Fix EBS access

--- main.tf
+++ main.tf
@@ -26,6 +26,20 @@ locals {
   }
 }
 
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "${local.name}-ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.20.0"
@@ -37,6 +51,12 @@ module "eks" {
   cluster_addons = {
     coredns = {
       most_recent = true
+    }
+
+    # https://docs.aws.amazon.com/eks/latest/userguide/managing-ebs-csi.html#csi-iam-role
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+      most_recent              = true
     }
     kube-proxy = {
       most_recent = true
